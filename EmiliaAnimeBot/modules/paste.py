bbb181d176cdbfc07a545c5a90d4a97eeac529e2
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-10 21:30:15 +0530

    Update paste.py

--- EmiliaAnimeBot/modules/paste.py
+++ EmiliaAnimeBot/modules/paste.py
@@ -49,3 +49,7 @@ dispatcher.add_handler(PASTE_HANDLER)
 
 __command_list__ = ["paste"]
 __handlers__ = [PASTE_HANDLER]
+__help__ = """
+ • `/paste` *:* Saves replied content to `nekobin.com` and replies with a url
+"""
+__mod_name__ = "Paste"
